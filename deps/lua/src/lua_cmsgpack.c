e8eb33013970d3926cdeb8f42f067b80e2ec30cd
梁军 <liangjun@aurogon.com>
2017-02-17 15:44:03 +0800

    fix int64 as table key cause cmsgpack pack error

--- deps/lua/src/lua_cmsgpack.c
+++ deps/lua/src/lua_cmsgpack.c
@@ -421,10 +421,11 @@ void mp_encode_lua_table_as_map(lua_State *L, mp_buf *buf, int level) {
  * of keys from numerical keys from 1 up to N, with N being the total number
  * of elements, without any hole in the middle. */
 int table_is_an_array(lua_State *L) {
-    int count = 0, max = 0;
 #if LUA_VERSION_NUM < 503
+    int count = 0, max = 0;
     lua_Number n;
 #else
+    lua_Integer count = 0, max = 0;
     lua_Integer n;
 #endif
 
